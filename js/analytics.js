f4f3b1831b8211398befe6386c71f27a0f8ed432
dngea <66570410+dngea@users.noreply.github.com>
2025-02-18 13:28:41 +0100

    analytics

---
+++ js/analytics.js
@@ -0,0 +1,14 @@
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-QT916BT4LX";
+    document.head.appendChild(script);
+  
+    script.onload = function() {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-QT916BT4LX');
+    };
+  })();
+  
